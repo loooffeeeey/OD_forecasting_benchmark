b9e3c2b6d41da60ded3207a6f55b9e5043471331
SHJ-JHS <992386702@qq.com>
2023-04-24 13:12:22 +0800

    GCRN

---
+++ models/GCRN/README.md
@@ -0,0 +1,10 @@
+# RSODP
+Origin-Destination Prediction for Ridesharing
+
+<br>
+
+<!-- <style>
+    body {
+        text-align: justify;
+    }
+</style> -->
---
+++ models/GCRN/utils/__init__.py
@@ -0,0 +1,2 @@
+from .Logger import Logger
+from .Utils import batch2device, RMSE, MAE, MAPE, plot_grad_flow, evalMetrics, genMetricsResStorage, aggrMetricsRes, wrapMetricsRes
